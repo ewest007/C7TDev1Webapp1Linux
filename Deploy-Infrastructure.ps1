1ada379cd2e39ce0ba84adb98144775cd78d0db1
Ewan Forbes <ewan_forbes@yahoo.co.uk>
2018-11-06 19:25:23 +0000

    Separated app and vnet templates

--- Deploy-Infrastructure.ps1
+++ Deploy-Infrastructure.ps1
@@ -1,20 +1,34 @@
 param(
     [parameter(Mandatory=$false)]
-    [string]$templatePath = ".\armTemplates\baseVm.json",
+    [string]$vmTemplatePath = ".\armTemplates\baseVm.json",
+    [parameter(Mandatory=$false)]
+    [string]$vnetTemplatePath = ".\armTemplates\baseVnet.json",
     [parameter(Mandatory=$false)]
     [string]$deploymentLocation = "North Europe",
     [parameter(Mandatory=$false)]
-    [string]$resourceGroupName = "infrastructure-rg"
+    [string]$resourceGroupName = "infrastructure-rg",
+    [parameter(Mandatory=$false)]
+    [pscredential]$adminCreds = $null,
+    [parameter(Mandatory=$false)]
+    [string]$vnetName = "vnet"
 )
 
 Write-Output "Please enter the credentials that will be used for the VM"
 
-$creds = Get-Credential
-
-$deploymentParams = @{ "adminUsername" = $creds.UserName; "adminPassword" = $creds.Password}
+if ($null -eq $adminCreds) {
+    $adminCreds = Get-Credential
+}
 
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $deploymentLocation
 
+$vnetDeploymentParams = @{ "vnetName" = $vnetName }
+
+$vnet = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name "deploy-infrastructure" `
+                                    -TemplateUri $vnetTemplatePath -TemplateParameterObject $vnetDeploymentParams `
+                                    -Verbose
+
+$vmDeploymentParams = @{ "adminUsername" = $adminCreds.UserName; "adminPassword" = $adminCreds.Password; "vnetID" = $vnet.outputs.vnetID.value}
+
 New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name "deploy-infrastructure" `
-                                    -TemplateUri $templatePath -TemplateParameterObject $deploymentParams `
+                                    -TemplateUri $vmTemplatePath -TemplateParameterObject $vmDeploymentParams `
                                     -Verbose
